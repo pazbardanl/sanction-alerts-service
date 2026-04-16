06e28ce39172d651b4cee0b50580ecd9f02b3f8a
pazbarda-nsknox <paz.barda@nsknox.net>
2026-04-16 22:03:35 +0300

    Blank spring boot application, gradle

---
+++ src/main/java/com/fincom/sanction/AlertsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fincom.sanction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fincom/sanction/AlertsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fincom.sanction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
